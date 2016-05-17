7a308b54145447801a99610b25a071593a14284f
luciano coccia <lcoccia@mac-lcoccia.lan>
2016-05-17 23:11:28 +0200

    Now the fractions are normalized

--- src/main/java/com/nerdkapp/fractions/Fraction.java
+++ src/main/java/com/nerdkapp/fractions/Fraction.java
@@ -1,34 +1,59 @@
 package com.nerdkapp.fractions;
 
 public class Fraction {
-  private Integer value;
+  private Integer numerator;
+  private Integer denominator;
 
   public Fraction(Integer value) {
-    this.value = value;
+    this.numerator = value;
+    this.denominator = 1;
+  }
+
+  public Fraction(Integer numerator, Integer denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
   }
 
   public Fraction add(Fraction fraction) {
-    return new Fraction(this.value + fraction.getValue());
+    return minimize(new Fraction(this.numerator + fraction.getNumerator(), this.denominator));
   }
 
-  public Integer getValue() {
-    return value;
+  private Fraction minimize(Fraction fraction) {
+    Integer gcd = gcd(fraction.numerator, fraction.denominator);
+    return new Fraction(fraction.numerator/gcd, fraction.denominator/gcd);
+
+  }
+
+  private Integer gcd(Integer a, Integer b) {
+    if (b==0) return a;
+    return gcd(b, a % b);
+  }
+
+  public Integer getDenominator() {
+    return denominator;
+  }
+
+  public Integer getNumerator() {
+    return numerator;
   }
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    Fraction otherFraction = minimize((Fraction) o);
+    Fraction thisFraction = minimize(this);
 
-    Fraction fraction = (Fraction) o;
-
-    if (value != null ? !value.equals(fraction.value) : fraction.value != null) return false;
-
-    return true;
+    return thisFraction.numerator.equals(otherFraction.numerator) && thisFraction.denominator == otherFraction.denominator;
   }
 
   @Override
   public int hashCode() {
-    return value != null ? value.hashCode() : 0;
+    int result = numerator != null ? numerator.hashCode() : 0;
+    result = 31 * result + (denominator != null ? denominator.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return numerator + "/" + denominator;
   }
 }
--- src/test/java/FractionsTest.java
+++ src/test/java/FractionsTest.java
@@ -4,6 +4,7 @@ import com.nerdkapp.fractions.Fraction;
 import org.junit.Test;
 
 public class FractionsTest {
+  
   @Test
   public void add_zero_to_zero() throws Exception {
     Fraction result = new Fraction(0).add(new Fraction(0));
@@ -15,4 +16,26 @@ public class FractionsTest {
     Fraction result = new Fraction(1).add(new Fraction(1));
     assertEquals(new Fraction(2),result);
   }
+
+  @Test
+  public void half_plus_half(){
+    Fraction result = new Fraction(1,2).add(new Fraction(1,2));
+    assertEquals(new Fraction(2,2), result);
+  }
+
+  @Test
+  public void half_plus_half_shouldbe_normalized(){
+    Fraction result = new Fraction(1,2).add(new Fraction(1,2));
+    assertEquals(new Fraction(1), result);
+  }
+
+//  @Test
+//  public void testGCD() throws Exception {
+//    System.out.println(GCD(4,2));
+//
+//  }
+
+
 }
+
+//normalizzazione (2/4+2/4)
