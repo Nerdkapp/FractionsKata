e1f712df6ce62463e85138d764a65d37cbe60da8
luciano coccia <lcoccia@mac-lcoccia.lan>
2016-05-17 22:34:51 +0200

    First commit

---
+++ src/test/java/FractionsTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FractionsTest {
+  @Test
+  public void test_zero() throws Exception {
+    assertEquals(0, 1);
+  }
+}
