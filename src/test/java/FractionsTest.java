2630959b0969ab6191804fb1b0b10b127eda2dca
Luciano Coccia <luciano.coccia@lastminute.com>
2016-05-20 13:48:47 +0200

    Test github desktop
    This is a test commit

--- src/test/java/FractionsTest.java
+++ src/test/java/FractionsTest.java
@@ -4,7 +4,7 @@ import com.nerdkapp.fractions.Fraction;
 import org.junit.Test;
 
 public class FractionsTest {
-  
+
   @Test
   public void add_zero_to_zero() throws Exception {
     Fraction result = new Fraction(0).add(new Fraction(0));
